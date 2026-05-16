90a63025ccb98db9e3c31d2c7b585203253d6817
amytzeng <tzengamy@gmail.com>
2026-05-16 17:31:37 +0800

    set name: admin

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,15 +1,37 @@
 import { createBrowserRouter } from 'react-router-dom'
+import RequireAuth from '@/components/auth/RequireAuth'
+import OnboardingStepGuard from '@/components/auth/OnboardingStepGuard'
 import App from './App'
+import OnboardingLayout from './layouts/OnboardingLayout'
 import DashboardPage from './pages/DashboardPage'
 import OrdersPage from './pages/OrdersPage'
 import MessagesPage from './pages/MessagesPage'
 import StatsPage from './pages/StatsPage'
 import LoginPage from './pages/LoginPage'
+import OnboardingNamePage from './pages/onboarding/OnboardingNamePage'
 
 export const router = createBrowserRouter([
   {
     path: '/login',
     element: <LoginPage />,
+  },
+  {
+    path: '/onboarding',
+    element: (
+      <RequireAuth>
+        <OnboardingLayout />
+      </RequireAuth>
+    ),
+    children: [
+      {
+        path: 'name',
+        element: (
+          <OnboardingStepGuard expectedStep="NAME">
+            <OnboardingNamePage />
+          </OnboardingStepGuard>
+        ),
+      },
+    ],
   },
   {
     path: '/',
