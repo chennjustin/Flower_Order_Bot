13444a8f3483050601782f62224a827edd3bab4c
ccloudinthesky <alinartwice@gmail.com>
2026-06-06 10:02:55 +0800

    feat: remove StorePicker from Navbar and adjust CalendarView styles for consistency

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import BrandLogo from './BrandLogo'
 import Sidebar from './Sidebar'
 import SidebarMenuIcon from './SidebarMenuIcon'
-import StorePicker from './StorePicker'
-
 export default function Navbar() {
   const [showSidebar, setShowSidebar] = useState(false)
 
@@ -21,9 +19,6 @@ export default function Navbar() {
         </button>
         <div className="absolute left-[73px] top-1/2 flex -translate-y-1/2 items-center gap-[13px]">
           <BrandLogo size="nav" />
-        </div>
-        <div className="absolute right-4 top-1/2 flex -translate-y-1/2 items-center">
-          <StorePicker />
         </div>
       </header>
 
